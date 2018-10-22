22fb30bef8284f5cfe42b99eba207ca6b32aa0db
Benjamin CHEN <BenjaminCHEN2016@outlook.com>
2018-10-22 10:35:08 +0800

    阿宁啾啾

--- homework2_陈弈君_10152160137.v
+++ homework2_陈弈君_10152160137.v
@@ -59,107 +59,41 @@ Qed.
 
 (* 4. Theorem mult_comm : forall m n : nat, m * n = n * m. *)
 
-Theorem plus_0_r : forall n:nat, n + 0 = n.
-Proof.
-  intros n. induction n as [| n'].
+Theorem mult_n_O : forall n : nat,  n * 0 = 0.
+Proof. 
+  intros n. induction n as [| n' IHn'].
   - simpl. reflexivity.
-  - simpl. rewrite -> IHn'. reflexivity.  Qed.
-
-Theorem plus_assoc : forall n m p : nat,
-  n + (m + p) = (n + m) + p.
-Proof.
-  intros.
-  induction n.
-  - reflexivity.
-  - simpl.
-    rewrite -> IHn.
-    reflexivity. 
-  Qed.
-
-Lemma plus_distr : forall n m: nat, S (n + m) = n + (S m).
-  Proof.
-    intros. 
-    induction n.
-    reflexivity.
-    simpl.
-    rewrite -> IHn. 
-    reflexivity. 
-  Qed.
-
-Theorem plus_comm : forall n m : nat,
-  n + m = m + n.
-Proof.
-  intros.
-  induction n.
-  - simpl. rewrite -> plus_0_r. reflexivity.
-  - simpl. rewrite ->IHn. rewrite -> plus_distr. reflexivity.
-  Qed.
-
-Theorem plus_swap : forall n m p : nat, 
-  n + (m + p) = m + (n + p).
-Proof.
-  intros n m p.
-  rewrite -> plus_assoc.
-  assert(H: m + (n + p) = (m + n) + p).
-  { rewrite -> plus_assoc. reflexivity. }
-  rewrite -> H.
-  assert(H2: m + n = n + m).
-  { rewrite -> plus_comm. reflexivity. }
-  rewrite -> H2.
-  reflexivity.
-  Qed.
-
-
-Lemma mult_distr : forall n m: nat, (n*m) + n = n*(S m).
-  Proof.
-    intros. 
-    induction n.
-    - reflexivity.
-    - simpl. rewrite <- plus_n_Sm. rewrite <- IHn. rewrite -> plus_assoc. reflexivity. 
-  Qed.
-
-Lemma mult_distr2 : forall n m: nat, (n*m) + m = (S n)*m.
-  Proof.
-    intros. 
-    induction n.
-    simpl.
-    rewrite -> plus_0_r.
-    reflexivity.
-    simpl.
-    rewrite -> plus_assoc.
-    assert (n*m + m = m + n*m).
-      rewrite -> plus_comm.
-      reflexivity.
-    rewrite <- H.
-    rewrite <- plus_comm with (n := n*m) (m := m + m).
-    rewrite -> plus_assoc.
-    reflexivity. 
-  Qed.
-
-
-Theorem mult_0_r : forall n:nat,
-  n * 0 = 0.
-Proof.
-  intros.
-  induction n.
-  simpl.
-  reflexivity.
-  simpl.
-  apply IHn.
-  Qed.
-
-Theorem mult_comm : forall m n : nat,
- m * n = n * m.
-Proof.
-  intros.
-  induction n as [| n'].
-  (* n = 0 *)
-    simpl.
-    rewrite -> mult_0_r.
-    reflexivity.
-  (* n = S n' *)
-    rewrite <- mult_distr.
-    rewrite -> IHn'.
-    rewrite -> mult_distr2.
-    reflexivity.
+  - simpl. rewrite IHn'. reflexivity.
 Qed.
+
+Theorem plus_n_O : forall n:nat, n = n + 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - (* n = 0 *)    reflexivity.
+  - (* n = S n' *) simpl. rewrite <- IHn'. reflexivity.  Qed.
+
+Theorem mult_n_1 : forall n:nat, n * 1 = n.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite IHn'. reflexivity.  Qed.
+
+Theorem mult_comm_pre : forall m n : nat, n + n * m = n * S m.
+Proof. 
+  intros n m. induction m as [|n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite <- IHn'. rewrite plus_comm_assoc. reflexivity.
+Qed.
+
+Theorem mult_comm : forall m n : nat,  m * n = n * m.
+Proof. 
+  intros m n. induction m as [|m' IHm'].
+  - simpl. rewrite mult_n_O. reflexivity.
+  - simpl. rewrite IHm'. rewrite mult_comm_pre. reflexivity.
+Qed.
+  
+
+
+
+
+
